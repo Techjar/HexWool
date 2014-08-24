209a239629707a504986c5492620f239f79b1240
Techjar <tecknojar@gmail.com>
2014-08-24 03:31:22 -0400

    Clean up more old commented code

--- src/main/java/com/techjar/hexwool/item/ItemBlockColoredWool.java
+++ src/main/java/com/techjar/hexwool/item/ItemBlockColoredWool.java
@@ -16,7 +16,6 @@ import cpw.mods.fml.relauncher.SideOnly;
 public class ItemBlockColoredWool extends ItemBlock {
 	public ItemBlockColoredWool(Block block) {
 		super(block);
-		// this.setUnlocalizedName("hexwool.block.coloredWool");
 	}
 
 	@Override
@@ -27,13 +26,4 @@ public class ItemBlockColoredWool extends ItemBlock {
 			list.add(StatCollector.translateToLocal("hexwool.string.colorText") + ": #" + (color == -1 ? "EASTER" : Util.colorToHex(color)));
 		}
 	}
-
-	/*
-	 * @SideOnly(Side.CLIENT) public int getColorFromItemStack(ItemStack
-	 * itemStack, int par2) { //throw new RuntimeException("where are we");
-	 * //System.out.println("YO DIGGA IN THE SHIZZLE I GOT RIZZLED"); int ret =
-	 * 16777215; if (itemStack.hasTagCompound()) { ret =
-	 * itemStack.getTagCompound().getInteger("color"); }
-	 * BlockColoredWool.mostRecentColor = ret; return ret; }
-	 */
 }
